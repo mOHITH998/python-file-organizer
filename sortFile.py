13915b10389bb0e1f8f07352b69a723eb2e98625
mOHITH998 <mohithg98@gmail.com>
2023-07-22 12:37:54 +0530

    first commit

---
+++ sortFile.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from shutil import move, copytree
+arrList = os.listdir()
+path = os.getcwd()
+# newPath = '\py\Downloads\Organized'+'\Organized'
+
+extensions = {
+    ".zip": "ZipFile",
+    ".pdf": "Documents",
+    ".mp3": "Audio",
+    ".jpg": "Images",
+    ".jpeg": "Pictures",
+    ".docx": "Word",
+    ".py": "Python",
+    ".js": "Javascript",
+    ".html": "HTML",
+    ".mp4": "Videos",
+    ".mkv": "Videos"
+}
+
+for key in extensions:
+    for file in arrList:
+        file_path = os.path.join(path, file)
+        newPath = os.path.join(path, extensions[key])
+
+        # To Find the current working script/file name
+        script_file_name = os.path.basename(sys.argv[0])
+        # This condition is to filter the working file/script from moving/creating the directory
+        if (file != script_file_name):
+            if (file.endswith(key)):
+                if not os.path.exists(newPath):
+                    os.mkdir(newPath)
+                else:
+                    print("folder %s already exists" % newPath)
+            # directory = os.mkdir(newPath)
+            # destination = os.path.join(newPath, extensions[key])
+            # # destination = os.path.join(newPath, extensions[key])
+                print('-'*50)
+                print("File '%s' moved Sucessfully to '%s' Directory." %
+                      (file, extensions[key]))
+                print('-'*50)
+                move(file_path, newPath)
